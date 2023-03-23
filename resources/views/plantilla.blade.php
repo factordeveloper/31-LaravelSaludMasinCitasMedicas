2eda914bff9995b4227502c1298da4f1c3077799
Felipe Herrera Pinzón <felipeherrerapinzon@gmail.com>
2023-03-23 13:45:20 -0500

    updated

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,26 +8,26 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/font-awesome/css/font-awesome.min.css">
   
   <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/Ionicons/css/ionicons.min.css">
   
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/dist/css/skins/_all-skins.min.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/morris.js/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/jvectormap/jquery-jvectormap.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/bower_components/bootstrap-daterangepicker/daterangepicker.css">
  
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,19 +38,19 @@
 
 <!-- DataTables -->
 
-<link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+<link rel="stylesheet" href="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
  <!-- <link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">  -->
 
 <!-- Full Calendar  -->
 
-<link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/fullcalendar/dist/fullcalendar.min.css">
-<link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/fullcalendar/dist/fullcalendar.print.min.css" media="print">
+<link rel="stylesheet" href="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/fullcalendar/dist/fullcalendar.min.css">
+<link rel="stylesheet" href="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/fullcalendar/dist/fullcalendar.print.min.css" media="print">
 
 
 <!-- Select 2 -->
 
-<link rel="stylesheet" href="http://localhost/SaludMasinCitasMedicas/public/bower_components/select2/dist/css/select2.min.css">
+<link rel="stylesheet" href="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/select2/dist/css/select2.min.css">
 
 <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
@@ -92,51 +92,51 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/raphael/raphael.min.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/morris.js/morris.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/raphael/raphael.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/morris.js/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/moment/min/moment.min.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/moment/min/moment.min.js"></script>
+<script src="http://localhost/SaludMasinCitasMedicas/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 <!-- Slimscroll -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/dist/js/adminlte.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/dist/js/pages/dashboard.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/dist/js/demo.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/dist/js/demo.js"></script>
 <!-- DataTables -->
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/datatables.net-bs/js/dataTables.responsive.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/datatables.net-bs/js/dataTables.responsive.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
 <!-- Full Calendar  -->
 
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/fullcalendar/dist/locale/es.js"></script>
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/moment/moment.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/fullcalendar/dist/locale/es.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/moment/moment.js"></script>
 
 <!-- Select2  -->
 
-<script src="http://localhost/SaludMasinCitasMedicas/public/bower_components/select2/dist/js/select2.js"></script>
+<script src="https://laravelsaludmasincitasmedicas-production.up.railway.app/bower_components/select2/dist/js/select2.js"></script>
 
 <script type="text/javascript">
   $(".table").DataTable({
